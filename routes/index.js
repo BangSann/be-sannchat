21bbd3ad06fee5da250cd0b11c17bda5c4871e66
BangSann <yosanokta12@gmail.com>
2025-11-02 20:39:05 +0700

    fix: coba update docker

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var messageRouter = require("./messages");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express v.2" });
 });
 
 router.use("/users", userRoute);
